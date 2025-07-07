4e50fb908682f0404d19216b313c93aaba2cec9d
an-tonDev <ayomideobaroovooboh@gmail.com>
2025-07-07 23:47:33 +0100

    Initial commit: Expense Tracker setup

---
+++ .history/dev-data/importDevData_20250707233228.js
@@ -0,0 +1,42 @@
+const fs=require('fs')
+const dotenv=require('dotenv')
+const mongoose=require('mongoose')
+const User= require('../models/userModel')
+const Transaction= require('../models/transactionModel')
+
+dotenv.config({ path: './config.env' });
+
+const DB=process.env.DATABASE
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+
+
+const users=JSON.parse(fs.readFileSync(`${__dirname}/users.json`,'utf-8'))
+const transactions=JSON.parse(fs.readFileSync(`${__dirname}/transaction.json`,'utf-8'))
+
+const importData = async () =>{
+    try{
+    await User.create(users)
+    await Transaction.create(transactions)
+    }catch(err){
+    console.log(err.message)
+    }
+    process.exit()
+}
+
+const deleteData = async ()=>{
+    try{
+    await User.deleteMany()
+    await Transaction.deleteMany()
+    }catch(err){
+    console.log(err.message)
+    }
+    process.exit()
+}
+
+if(process.argv[2]==='--import'){
+    importData()
+}else if(process.argv[2]==='--delete'){
+    deleteData()
+}
