f4f8636c17e03cbed0a44ee08df98bf9d1a5fef8
anthony <ayomideobaroovooboh@gmail.com>
2025-09-03 21:39:21 +0100

    code refactoring

--- .history/routes/userRoutes_20250903194949.js
+++ .history/routes/userRoutes_20250903194949.js
@@ -2,17 +2,18 @@ const express= require('express')
 const userController= require('../controllers/userController')
 const authController= require('../controllers/authController')
 
-
-
 const router=express.Router()
+router.use(authController.protect)
 
 router
 .route('/')
 .get(userController.getUsers)
-.post(userController.createUser)
+.post(authController.restrictTo('admin'),userController.createUser)
 
 router
 .route('/:id')
 .get(userController.getUser)
-.patch(userController.updateUser)
-.delete(userController.deleteUser)
+.patch(authController.restrictTo('admin'),userController.updateUser)
+.delete(authController.restrictTo('admin'),userController.deleteUser)
+
+module.exports=router
